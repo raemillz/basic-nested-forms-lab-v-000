d1e11b87442c2538950d69b9a65ada11c70e4b4f
Rachel Miller <raeanne1991@gmail.com>
2018-03-23 19:20:02 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients
   accepts_nested_attributes_for :ingredients
   validates :title, presence: true
+  validates :ingredient_attributes, presence: true
 end
